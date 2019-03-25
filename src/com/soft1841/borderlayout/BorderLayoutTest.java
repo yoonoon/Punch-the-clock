9dd87195568a4611576b168f00fb4632db9415a4
yoonoon <2075530188@qq.com>
2019-03-26 06:34:24 +0800

    borderlayout

---
+++ src/com/soft1841/borderlayout/BorderLayoutTest.java
@@ -0,0 +1,34 @@
+package com.soft1841.borderlayout;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BorderLayoutTest extends JFrame {
+    public BorderLayoutTest(){
+        setTitle("边界布局管理器");
+        //定义一个容器
+        Container c = getContentPane();
+        //设置容器为边界布局管理器
+        setLayout(new BorderLayout());
+        JButton centerBtn = new JButton("中神通"),
+                northBtn = new JButton("北乞"),
+                southBtn = new JButton("南帝"),
+                westBtn = new JButton("西毒"),
+                eastBtn = new JButton("东邪");
+        //中间添加按钮
+        c.add(centerBtn,BorderLayout.CENTER);
+        c.add(northBtn,BorderLayout.NORTH);
+        c.add(southBtn,BorderLayout.SOUTH);
+        c.add(westBtn,BorderLayout.WEST);
+        c.add(eastBtn,BorderLayout.EAST);
+        //设置窗体大小
+        setSize(320,200);
+        //设置窗体可见
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new  BorderLayoutTest();
+    }
+}
